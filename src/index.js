4f61df3deea3b7ac7ab3dc94a04d7d4adabe235a
Yen Tran <tran.yentran@gmail.com>
2017-11-06 11:36:22 -0800

    Youtube Search Response

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
 const API_KEY = config.API_KEY;
 
-//Create a new component. This component should produce some HTML
+/* YOUTUBE Search Response
+*   - Downwards data flow: only the most parent component in the application should be responsible for fetching data
+*/
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
 
 const App = () => {
     return (
@@ -14,5 +22,4 @@ const App = () => {
     )
 }
 
-//Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
