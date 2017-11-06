c3c147cb9b678ea115138a32399f63527680412a
Yen Tran <tran.yentran@gmail.com>
2017-11-06 11:46:45 -0800

    Refactoring Functional Components to Class Components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = config.API_KEY;
 
-/* YOUTUBE Search Response
-*   - Downwards data flow: only the most parent component in the application should be responsible for fetching data
+/* Refactoring Functional Component to Class-based Component
+*   -We want to do this to App because we want app to keep track of the list of videos. Where we want to do this is in it's state
 */
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { videos: [] };
 
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            // non-ES6: this.setState({videos: videos})
+            // ES6: only when key and values are the same
+            this.setState({videos})
+        });
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        )
+    }
+}
+
+
+/*
+OLD CODE:
 const App = () => {
     return (
         <div>
@@ -21,5 +38,6 @@ const App = () => {
         </div>
     )
 }
+*/
 
 ReactDOM.render(<App />, document.querySelector('.container'));
