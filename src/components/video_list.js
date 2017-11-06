96963630299e69ad5403ef1aee681f8e173fb2c1
Yen Tran <tran.yentran@gmail.com>
2017-11-06 12:50:12 -0800

    Video List Items

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,17 +3,9 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
 
-    /*
-* - Whenever we render and array of items of the same type, React correctly assumes that we're building a list.
-* - React has a bunch of logic built into it to optimize the process of rendering a list. Because it's storing the list in a hash-like object, it needs keys to access each list item.
-* - A common strategy when rendering a list of data is to use the ID for that piece of data. In this example, we will use Youtube's 'etag'.
-* */
-
     const videoItems = props.videos.map((video) => {
         return <VideoListItem key={video.etag} video={video}/>
     });
-
-
 
     return (
         <ul className="col-md-4" list-group>
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+/*ES6 Refactor:*/
+const VideoListItem = ({video}) => {
+    const imageUrl = video.snippet.thumbnails.default.url;
+
+    return (
+        <li className='list-group-item'>
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl}/>
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">
+                        {video.snippet.title}
+                    </div>
+                </div>
+            </div>
+        </li>
+    );
+};
+
+/* Non-ES6
 const VideoListItem = (props) => {
-    return <li>Video</li>
+    const video = props.video;
+    ...
 }
+*/
 
 export default VideoListItem
