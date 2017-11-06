cff9076074d884f50b8932928e71d3cd02832053
Yen Tran <tran.yentran@gmail.com>
2017-11-06 10:19:39 -0800

    adds new component files, adds config for API_KEY

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = config.API_KEY;
+
 //Create a new component. This component should produce some HTML
 
 const App = () => {
