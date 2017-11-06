9b29f98ceb8de3bc0f4a2745d4ce42721394ffb4
Yen Tran <tran.yentran@gmail.com>
2017-11-06 10:45:40 -0800

    Handling User Events

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 
-//***functional component
-// const SearchBar = () => {
-//     return <input />
-// }
+/* Steps to Handling User Events
+* 1. Declare a event handler - a function that should be ran whenever the event occurs
+* 2. Pass the event handler to the element that we want to monitor for events
+* */
 
-//****class component => more functionality
 class SearchBar extends Component {
-    //every component requires a render method
     render () {
-        return <input />
+        //pass event handler to element
+        return <input onChange={ this.onInputChange } />
+    }
+
+    //Event Handler
+    onInputChange(event) {
+        console.log(event.target.value);
     }
 }
 
 export default SearchBar;
+
+
+/****** REFACTOR USING ES6:
+class SearchBar extends Component {
+    render () {
+        return <input onChange={event => console.log(event.target.value)} />
+    }
+}
+*/
