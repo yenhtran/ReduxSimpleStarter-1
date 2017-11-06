94c9a57040d333c6b8ccc81b274f159d03601a63
Yen Tran <tran.yentran@gmail.com>
2017-11-06 09:32:23 -0800

    replace original src folder

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+//Create a new component. This component should produce some HTML
 
-import App from './components/app';
-import reducers from './reducers';
+//Take this component's generated HTML and put it on the page (in the DOM)
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
