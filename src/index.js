32ae300cb90bb7394a8a31759bf91145e6d655a7
Yen Tran <tran.yentran@gmail.com>
2017-11-06 10:03:55 -0800

    basic React render 'Hi'

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 //Create a new component. This component should produce some HTML
 
-//Take this component's generated HTML and put it on the page (in the DOM)
+const App = () => {
+    return <div>Hi!</div>
+}
 
+//Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
